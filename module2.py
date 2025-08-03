21638fe4785fe922b279da8130be2893c32035aa
Chethanrc <151119842+Chethanrc@users.noreply.github.com>
2025-08-03 23:26:28 +0530

    Add files via upload

---
+++ module2.py
@@ -0,0 +1,37 @@
+# for addition
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+addition=a+b
+print("addition:",addition)
+
+# for subtraction
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+sub=a-b
+print("subtrction:",sub)
+
+
+# for multiplication
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+mul=a*b
+print("multiplication:",mul)
+
+# for division
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+if b != 0:
+    div=a/b
+    print("division:",div)
+else:
+    print("Error: Division by zero is not allowed.")
+    
+    
+    
+# Task 2
+
+a=input("enter the first name:")
+b=input("enter the second name:")
+name=a+b
+print("hello,",name+"!"+"  Welcome to python programing")
+    
